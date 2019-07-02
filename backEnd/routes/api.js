c9584b4c6d4219eac1b18ed664b51c486e932c41
narendram224 <narendram224.nm@gmail.com>
2019-07-02 11:26:25 +0530

    crud api done

--- backEnd/routes/api.js
+++ backEnd/routes/api.js
@@ -30,6 +30,88 @@ router.post('/register',(req,res)=>{
 
 
 router.post('/login',(req,res)=>{
-
+        let data= req.body;
+        let user= new User(data);
+         // log in page
+         User.findOne({email:data.email},(err,existUser)=>{
+              if (err){
+                  res.send(err);
+              }else if(existUser){
+                    let password = data.password;
+                        if(existUser.password == password){
+                            res.status(200).send({user:existUser,
+                            message:"login succesfull"});
+                        }
+                        else{
+                            res.status(401).send("paasword is wrong");                        }
+                }else{
+                     res.status(400).send("email is invalid");  
+                    }
+                
+         });
 });
+
+
+// events 
+
+router.get('/event',(req,res) =>{
+    let events = [
+                {
+                "id":"1",
+                "name":"auto",
+                "description":"loremerr",
+                "date":"2012-04-23T18:25:34.332Z"
+                },
+                {
+                    "id":"2",
+                    "name":"autdfo",
+                    "description":"loremefgfrr",
+                    "date":"2012-04-23T18:25:34.332Z"
+                },
+                {
+                    "id":"3",
+                    "name":"ariedfsuto",
+                    "description":"lovbremerr",
+                    "date":"2012-04-23T18:25:34.332Z"
+                },
+                {
+                    "id":"4",
+                    "name":"merw",
+                    "description":"loremerr",
+                    "date":"2012-04-23T18:25:34.332Z"
+                }
+            ]
+    res.json(events)
+});
+
+
+router.put('/:_id',(req,res)=>{
+
+            User.findByIdAndUpdate(req.params._id,{name:req.body.name},{useFindAndModify:false},(err,doc)=>{
+                    if (err) {
+                        res.json({error:err})
+                    } else {
+                            res.status(200).send({
+                                message:"succesfuly update ",
+                                response:doc
+                            })
+                    }
+            })
+
+        });
+
+router.delete('/:_id',(req,res)=>{
+            User.findByIdAndRemove(req.params._id,{useFindAndModify:false},(err,result)=>{
+                        if (err) {
+                            res.send(err);
+                        } 
+                        else if(result){
+                            res.status(200).json({message:"successful",
+                            response:result
+                            });       
+                        }else {
+                            res.status(401).send("invalid id "); 
+                        }
+            })   
+})
 module.exports = router;
